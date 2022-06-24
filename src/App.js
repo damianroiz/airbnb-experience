d601843e71d28c91dd864f3166f27172fe10fc60
Damian Roiz <damianx989@gmail.com>
2022-06-24 10:11:11 -0400

    fixed index file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function App() {
     return (
-        <h1>App Component</h1>
+        <h1>This is an app component</h1>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import  ReactDOM from "react-dom";
 import App from './App'
+import './style.css'
 
 const targetElement = document.getElementById("root")
 ReactDOM.render(<App />, targetElement)
